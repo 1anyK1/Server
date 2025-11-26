b8ed9bb0d2a9e7e39b9323e89f776083a69ee9b5
ia332s01 <ia332s01@csc.sibsutis.ru>
2025-11-26 10:04:04 +0700

    Add data base

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 import zmq
 import json
+import psycopg2
 
 BIND_ADDRESS = "tcp://*:5555"
 OUTPUT_FILENAME = "locations.json"
 
 def main():
+    conn = psycopg2.connect(dbname = "db_server", host= "localhost", user="postgres", password="postgres1234", port="5432")
+
+    print("Database connected")
+
+    curs = conn.cursor()
+
     context = zmq.Context()
     
     socket = context.socket(zmq.PULL)
@@ -20,18 +27,23 @@ def main():
     while True:
         try:
             message_bytes = socket.recv()
-            message = message_bytes.decode('utf-8')
-            try:
-                with open(OUTPUT_FILENAME, 'a', encoding='utf-8') as f:
-                    f.write(message + '\n') 
-                print("Получены и сохранены новые GPS-данные)")
-            except json.JSONDecodeError:
-                print("Пришли неверные данные")
+            data = json.loads(message_bytes)
+            lat = data['latitude']
+            lon = data['longitude']
+            timest = data['timestamp']
+
+            curs.execute("INSERT INTO user_info (Lat, Lon, Timestamp) values (%s, %s, %s)", (lat, lon, timest))
+            conn.commit()
+
+            print("Server recv data")
+
         except KeyboardInterrupt:
             print("\nСервер остановлен пользователем.")
             break
     socket.close()
     context.term()
+    curs.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
